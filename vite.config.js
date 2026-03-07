f9fae2e8c7e786ac05d23027abac2c7d2f8bb91d
aniket <aniket@Mac.lan>
2026-03-07 16:05:38 +0530

    Fix vite proxy rule intercepting files starting with hook

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
         target: 'http://localhost:3001',
         changeOrigin: true,
       },
-      '/hook': {
+      '/hook/': {
         target: 'http://localhost:3001',
         changeOrigin: true,
       },
